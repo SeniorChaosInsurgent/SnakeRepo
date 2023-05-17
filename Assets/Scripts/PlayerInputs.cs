d6fcd67bb60e702237e01c4dbb6cb92077a1aabf
ChaosInsurgency <generalov.vsevolod@gmail.com>
2023-05-17 22:00:20 +0200

    Updated SnakeMovementcs
     what did you expect, its in active development

---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour
+{
+    private Vector2 _movementDirection;
+
+    public Vector2 MovementDirection => _movementDirection;
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            _movementDirection = Vector2.right;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            _movementDirection = Vector2.left;
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            _movementDirection = Vector2.up;
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            _movementDirection = Vector2.down;
+        }
+    }
+}
--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -4,7 +4,33 @@ using UnityEngine;
 
 public class SnakeMovement : MonoBehaviour
 {
-    private Vector2 _direction;
+    private Vector2 _movementDirection;
 
+    [SerializeField] private float _speed;
 
+    private PlayerInputs _playerInputs;
+
+    private Rigidbody2D _rb2D;
+
+    private void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+        _playerInputs = GetComponent<PlayerInputs>();
+    }
+
+    private void FixedUpdate()
+    {
+        _movementDirection = _playerInputs.MovementDirection;
+
+        if (_rb2D != null)
+        {
+            _rb2D.position += _movementDirection * _speed;
+            Debug.Log(_movementDirection);
+        }
+        else
+        {
+            _rb2D = GetComponent<Rigidbody2D>();
+            Debug.LogWarning("No Rigidbody Present");
+        }
+    }
 }
