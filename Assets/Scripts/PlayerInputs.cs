6e4cdffd9fe03751ccc219368091b3194fc2dd06
ChaosInsurgency <generalov.vsevolod@gmail.com>
2023-05-22 21:20:12 +0200

    Update PlayerInputs.cs
    You cant make a 180% turn anymore

--- Assets/Scripts/PlayerInputs.cs
+++ Assets/Scripts/PlayerInputs.cs
@@ -8,22 +8,22 @@ public class PlayerInputs : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
+        if (Input.GetAxisRaw("Horizontal") > 0 && _movementDirection != Vector2.left)
         {
             _movementDirection = Vector2.right;
         }
 
-        if (Input.GetAxisRaw("Horizontal") < 0)
+        if (Input.GetAxisRaw("Horizontal") < 0 && _movementDirection != Vector2.right)
         {
             _movementDirection = Vector2.left;
         }
 
-        if (Input.GetAxisRaw("Vertical") > 0)
+        if (Input.GetAxisRaw("Vertical") > 0 && _movementDirection != Vector2.down)
         {
             _movementDirection = Vector2.up;
         }
 
-        if (Input.GetAxisRaw("Vertical") < 0)
+        if (Input.GetAxisRaw("Vertical") < 0 && _movementDirection != Vector2.up)
         {
             _movementDirection = Vector2.down;
         }
